573f44286c968797129acf56c8e81137372694ae
Leonardo <leonardoruiz1988@gmail.com>
2021-05-02 01:20:42 -0500

    Corrected #100, put if statement outside the print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
-    print(chr(i) if i % 2 == 0 else chr(i - 32), end="")
+    if i % 2 == 1:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
