8b27720e1d4989a110bd96d2d1e2bb0b6f4aee4b
Leonardo <leonardoruiz1988@gmail.com>
2021-07-05 16:54:10 -0500

    Modified single linked list on 100 taks

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,57 +1,47 @@
 #!/usr/bin/python3
-""" Module providing a 'Node' class for a singly-linked list
-"""
 
 
-class Node():
-    """ Definition of a singly-linked list node
-    """
+"""single link list module in python"""
+
+
+class Node:
+    """class Node for defining a single linked list"""
+
     def __init__(self, data, next_node=None):
-        """ Instantiate a node
-        """
-        self.data, self.next_node = data, next_node
+        """initialization for class node"""
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
-        """ Get the data stored in a node
-        """
-        return self.__data
-
-    @data.setter
-    def data(self, data):
-        """ Set the data stored in a node
-        """
-        if not isinstance(data, int):
-            raise TypeError("data must be an integer")
-        self.__data = data
+        """return data property when called"""
+        return (self._data)
 
     @property
     def next_node(self):
-        """ Get the next node
-        """
-        return self.__next_node
+        """returns next_node when called"""
+        return (self._next_node)
+
+    @data.setter
+    def data(self, value):
+        """sets the data for instance of Node"""
+        if type(value) == int:
+            self._data = value
+        else:
+            raise TypeError("data must be an integer")
 
     @next_node.setter
-    def next_node(self, next_node):
-        """ Set the next node
-        """
-        if next_node is not None and not isinstance(next_node, Node):
+    def next_node(self, value):
+        """sets the next_node for an instance of Node"""
+        if type(value) == Node or value == None:
+            self._next_node = value
+        else:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = next_node
 
+class SinglyLinkedList:
+    """defines a single linked list"""
 
-class SinglyLinkedList():
-    """ Definition of a singly-linked list
-    """
     def __init__(self):
-        """ Instantiate a singly-linked list
-        """
-        self.__head = None
-
-    def __str__(self):
-        """ Generate a visual representation of a list
-        """
+        self.head = None
 
     def sorted_insert(self, value):
-        """ Inset a Node into a list sorted in ascending order
-        """
